c00d68f6aa307e5907cff92df973ee05f6496f8a
Diabolo <alexis.boitel@epitech.eu>
2024-04-04 18:40:16 +0200

    feat(Displays): fix clocks

--- src/Displays/SFML/SFML.cpp
+++ src/Displays/SFML/SFML.cpp
@@ -141,7 +141,11 @@ void SFML::setAnimationTime(float time)
 
 float SFML::getDeltaT(void)
 {
-    return ((float)_lastTime / CLOCKS_PER_SEC);
+    clock_t currentTime = clock();
+    float deltaTime = static_cast<float>(currentTime - _lastTime) / CLOCKS_PER_SEC;
+    _lastTime = currentTime;
+    std::cout << deltaTime << std::endl;
+    return deltaTime;
 }
 
 void SFML::setText(std::string text, Arcade::Displays::Vector2i pos, Arcade::Displays::Color color)
