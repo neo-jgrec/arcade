d54b1715e4b2ff0c9287fa874b72cd28d7940f92
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2024-04-05 14:33:06 +0200

    feat(Snake): speed increase each time the length is increased

--- src/Games/Snake/Player/SnakePlayer.cpp
+++ src/Games/Snake/Player/SnakePlayer.cpp
@@ -75,7 +75,7 @@ void SnakePlayer::update(float deltaT)
     if (_moveCooldown > 0) {
         _moveCooldown -= deltaT;
     } else {
-        _moveCooldown = _speed;
+        _moveCooldown = _speed - (_length * 0.01);
 
         if (_rotation == ROTATION_UP) {
             goUp();
